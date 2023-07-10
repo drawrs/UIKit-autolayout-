aef9a901b30445fd6e2d948b0a57dd28c62061ec
drawrs <rizal.hilman67@gmail.com>
2023-07-10 14:34:37 +0700

    Initial Commit

---
+++ UIKit autolayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit autolayout
+//
+//  Created by Rizal Hilman on 10/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
